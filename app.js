59740cd9c90082c40c033fc3d1d30be890e72a9b
Irene <irene1martin2menendez@gmail.com>
2021-03-12 18:19:44 +0000

    timer and stop button fixed

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 $(function () {
     var bgImageArray = ["2015/06/08/15/19/waterfall-802003_1280.jpg", "2019/08/06/12/15/beach-4388225_1280.jpg", "2018/03/12/20/07/maldives-3220702_1280.jpg", "2020/03/03/20/31/boat-4899802_1280.jpg", "2021/03/04/19/01/lake-6069223_1280.jpg", "2016/10/18/21/28/canada-1751464_1280.jpg",
-        "2018/01/14/23/12/nature-3082832__480.jpg", "2016/11/29/02/20/astronomy-1866822_1280.jpg", "2016/02/09/19/57/aurora-1190254_1280.jpg", "2017/01/19/23/46/panorama-1993645_1280.jpg", "2016/12/11/12/02/bled-1899264__480.jpg", "2016/11/29/02/20/astronomy-1866822_1280.jpg",
+        "2018/01/14/23/12/nature-3082832__480.jpg", "2016/02/09/19/57/aurora-1190254_1280.jpg", "2017/01/19/23/46/panorama-1993645_1280.jpg", "2016/12/11/12/02/bled-1899264__480.jpg", "2016/11/29/02/20/astronomy-1866822_1280.jpg",
         "2016/10/27/19/31/greece-1776226__480.jpg", "2017/12/21/20/57/lavanttal-3032560__480.jpg", "2016/10/18/21/28/canada-1751464_1280.jpg", "2018/03/09/00/07/sand-3210284_1280.jpg", "2018/04/02/17/17/beach-3284446__480.jpg",
         "2015/12/08/00/34/ocean-waves-1081834_1280.jpg", "2017/03/13/11/44/island-2139457__480.jpg", "2016/01/08/18/00/antelope-canyon-1128815_1280.jpg", "2016/11/19/18/57/godafoss-1840758_1280.jpg", "2018/10/02/16/12/nature-3719233_1280.jpg", "2017/07/04/12/26/yosemite-2470909_1280.jpg",
-        "2017/01/07/14/34/moraine-lake-1960542_1280.jpg",
+        "2017/01/07/14/34/moraine-lake-1960542_1280.jpg", "2015/06/19/21/24/avenue-815297_1280.jpg", "2020/10/26/18/57/emerald-lake-5688358_1280.jpg", "2017/05/31/12/33/reflection-2360250_1280.jpg", "2019/07/20/20/11/nature-4351456_1280.jpg", "2016/10/06/03/17/moraine-lake-1718082_1280.jpg",
+        "2020/04/14/14/33/lake-5042933_1280.jpg", "2019/10/08/13/35/bergsee-4535036_1280.jpg", "2019/06/23/05/46/mountains-4292912_1280.jpg",
         "2018/10/14/11/12/seychelles-3746207__480.jpg", "2019/04/17/18/57/paradise-4135057__480.jpg", "2017/07/17/14/14/trees-2512497__480.jpg", "2016/02/04/20/41/scenery-1179905__480.jpg", "2021/03/04/19/01/lake-6069223__480.jpg",
         "2015/10/12/14/59/milky-way-984050__480.jpg", "2020/12/23/08/00/bow-lake-5854210__480.jpg", "2020/06/06/00/05/green-water-5264864__480.jpg", "2015/10/11/06/44/waterfall-981822__480.jpg"];
     base = "https://cdn.pixabay.com/photo/";
@@ -36,8 +37,10 @@ $(function () {
         }
     }
 
+    var interval;
+
     function timer() {
-        var interval = setInterval(function () {
+        interval = setInterval(function () {
             var minutesSelector = $('.minutes');
             var secondsSelector = $('.seconds');
             var minutes = minutesSelector.text();
@@ -48,35 +51,36 @@ $(function () {
             }
             minutesSelector.text(minutes);
             secondsSelector.text(seconds);
-            if (minutes === 0 && seconds === 0) {
+            if (minutes === '0' && seconds === 0) {
                 clearInterval(interval);
             }
         }, 1000);
     }
 
-    // function finish() {
-    //     $(".title").text("Well done!");
-    //     $(".animation").animate({marginLeft: "70%"}, 2000);
-    //     rollSound.pause();
-    //     soundBirds.pause();
-    //     soundRiver.pause();
-    //     soundNature.pause();
-    // }
-
-    function animationToTheLeft() {
-        $(".animation").animate({marginRight:"70%"} , 2000);
-        $(".card").animate({opacity: 1}, 2000);
-        $("button").text("Stop").addClass(".stop");
+    function finish() {
+        clearInterval(interval);
+        $(".title").text("Well done!");
+        $(".animation").animate({marginRight: "0"}, 2000);
+        rollSound.pause();
+        soundBirds.pause();
+        soundRiver.pause();
+        soundNature.pause();
     }
 
-    $('button').click(function () {
+    function animationToTheLeft() {
+        $(".animation").animate({marginRight: "70%"}, 2000);
+        $(".card").animate({opacity: 1}, 2000);
+    }
+
+    $(".start").click(function () {
         rollSound.play();
         backgroundSequence();
         timer();
         $(".title").text("Your time left.").animate({left: '250px'});
         $("p").hide().fadeOut('slow');
         animationToTheLeft();
-        $(".stop").click(finish());
+        $("button").text("Stop").removeClass("start").off('click').addClass("stop");
+        $(".stop").click(finish);
     });
 });
 
