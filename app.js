1a09e0dccf86d53dbb368d8d77352cc919a73a78
Irene <irene1martin2menendez@gmail.com>
2021-03-14 13:32:51 +0000

    fixed flashes in the background

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 $(function () {
     var bgImageArray = ["2015/06/08/15/19/waterfall-802003_1280.jpg", "2019/08/06/12/15/beach-4388225_1280.jpg", "2018/03/12/20/07/maldives-3220702_1280.jpg", "2020/03/03/20/31/boat-4899802_1280.jpg", "2021/03/04/19/01/lake-6069223_1280.jpg", "2016/10/18/21/28/canada-1751464_1280.jpg",
-        "2018/01/14/23/12/nature-3082832__480.jpg", "2016/02/09/19/57/aurora-1190254_1280.jpg", "2017/01/19/23/46/panorama-1993645_1280.jpg", "2016/12/11/12/02/bled-1899264__480.jpg", "2016/11/29/02/20/astronomy-1866822_1280.jpg",
+        "2018/01/14/23/12/nature-3082832__480.jpg", "2016/02/09/19/57/aurora-1190254_1280.jpg", "2017/01/19/23/46/panorama-1993645_1280.jpg", "2016/11/29/02/20/astronomy-1866822_1280.jpg",
         "2016/10/27/19/31/greece-1776226__480.jpg", "2017/12/21/20/57/lavanttal-3032560__480.jpg", "2016/10/18/21/28/canada-1751464_1280.jpg", "2018/03/09/00/07/sand-3210284_1280.jpg", "2018/04/02/17/17/beach-3284446__480.jpg",
         "2015/12/08/00/34/ocean-waves-1081834_1280.jpg", "2017/03/13/11/44/island-2139457__480.jpg", "2016/01/08/18/00/antelope-canyon-1128815_1280.jpg", "2016/11/19/18/57/godafoss-1840758_1280.jpg", "2018/10/02/16/12/nature-3719233_1280.jpg", "2017/07/04/12/26/yosemite-2470909_1280.jpg",
         "2017/01/07/14/34/moraine-lake-1960542_1280.jpg", "2015/06/19/21/24/avenue-815297_1280.jpg", "2020/10/26/18/57/emerald-lake-5688358_1280.jpg", "2017/05/31/12/33/reflection-2360250_1280.jpg", "2019/07/20/20/11/nature-4351456_1280.jpg", "2016/10/06/03/17/moraine-lake-1718082_1280.jpg",
-        "2020/04/14/14/33/lake-5042933_1280.jpg", "2019/10/08/13/35/bergsee-4535036_1280.jpg", "2019/06/23/05/46/mountains-4292912_1280.jpg",
-        "2018/10/14/11/12/seychelles-3746207__480.jpg", "2019/04/17/18/57/paradise-4135057__480.jpg", "2017/07/17/14/14/trees-2512497__480.jpg", "2016/02/04/20/41/scenery-1179905__480.jpg", "2021/03/04/19/01/lake-6069223__480.jpg",
+        "2020/04/14/14/33/lake-5042933_1280.jpg", "2019/10/08/13/35/bergsee-4535036_1280.jpg", "2019/06/23/05/46/mountains-4292912_1280.jpg", "2019/04/17/18/57/paradise-4135057__480.jpg", "2017/07/17/14/14/trees-2512497__480.jpg", "2021/03/04/19/01/lake-6069223__480.jpg",
+        "2016/10/18/21/28/seljalandsfoss-1751463_1280.jpg", "2017/10/10/07/48/beach-2836300_1280.jpg", "2016/02/19/10/35/nature-1209302_1280.jpg", "2015/07/02/10/22/stone-arch-828730_1280.jpg", "2017/08/27/15/09/moraine-lake-2686353_1280.jpg", "2018/01/19/09/31/kirkjufell-3092048_1280.jpg",
+        "2016/02/13/12/27/aurora-1197755_1280.jpg", "2014/09/27/17/30/waterfalls-463925_1280.jpg", "2016/11/29/13/04/forest-1869713_1280.jpg", "2016/11/18/14/17/geology-1834846_1280.jpg", "2020/11/22/07/11/river-5765785_1280.jpg",
+        "2015/11/07/11/30/sunbeam-1031201_1280.jpg",
         "2015/10/12/14/59/milky-way-984050__480.jpg", "2020/12/23/08/00/bow-lake-5854210__480.jpg", "2020/06/06/00/05/green-water-5264864__480.jpg", "2015/10/11/06/44/waterfall-981822__480.jpg"];
-    base = "https://cdn.pixabay.com/photo/";
-    secs = 4;
+    var base = "https://cdn.pixabay.com/photo/";
+    // secs = 4;
     bgImageArray.forEach(function (img) {
         new Image().src = base + img;
         // caches images, avoiding white flash between background replacements
@@ -19,38 +21,23 @@ $(function () {
     const soundRiver = new Audio("./Sound/River.wav");
     const rollSound = new Audio("./Sound/Music.wav");
 
-    // function backgroundSequence() {
-    //     window.clearTimeout();
-    //     var k = 0;
-    //     for (i = 0; i < bgImageArray.length; i++) {
-    //         setTimeout(function () {
-    //             document.body.style.background = "url(" + base + bgImageArray[k] + ") no-repeat";
-    //             document.body.style.backgroundSize = "cover";
-    //             if ((k + 1) === bgImageArray.length) {
-    //                 setTimeout(function () {
-    //                     backgroundSequence()
-    //                 }, (secs * 1000));
-    //             } else {
-    //                 k++;
-    //             }
-    //         }, (secs * 5000) * i);
-    //     }
-    // }
+    var backgroundInterval;
 
-    var i = 0;
-
-    function backgroundSequence(){
-    window.setInterval(function () {
-    $("body").css("backgroundSize", "cover").css("background","url(" + base + bgImageArray[i] + ") no-repeat")
-    var i = +1
-    }, 4000)
+    function backgroundSequence() {
+        backgroundInterval = setInterval(function () {
+            var randomNumber = Math.floor(Math.random() * bgImageArray.length);
+            const imageNext = base + bgImageArray[randomNumber];
+            $("body")
+                .css("background", "url(" + imageNext + ") no-repeat")
+                .css("backgroundSize", "cover").css("transition-duration", "5000");
+        }, 10000);
     }
 
 
-    var interval;
+    var timeInterval;
 
     function timer() {
-        interval = setInterval(function () {
+        timeInterval = setInterval(function () {
             var minutesSelector = $('.minutes');
             var secondsSelector = $('.seconds');
             var minutes = minutesSelector.text();
@@ -62,17 +49,18 @@ $(function () {
             minutesSelector.text(minutes);
             secondsSelector.text(seconds);
             if (minutes === '0' && seconds === 0) {
-                clearInterval(interval);
+                clearInterval(timeInterval);
             }
         }, 1000);
     }
 
     function finish() {
-        clearInterval(interval);
+        clearInterval(timeInterval);
         $(".title").text("Well done!");
         $(".animation").animate({marginRight: "0"}, 2000);
         $(".stop").css("display", "none");
         $(".start").css("display", "inline");
+        clearInterval(backgroundInterval);
         rollSound.pause();
         soundBirds.pause();
         soundRiver.pause();
